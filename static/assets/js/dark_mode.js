2d0f7e05880a985a426713cb631f78b34fc65707
0omanuo0 <manu365manu@gmail.com>
2024-06-11 11:05:45 +0000

    store theme in localstorage

--- static/assets/js/dark_mode.js
+++ static/assets/js/dark_mode.js
@@ -4,33 +4,47 @@ const switchButton2 = document.getElementById("mode-switch2");
 let is_dark = false;
 
 function checkDarkMode() {
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      document.documentElement.classList.add('dark');
-      is_dark = true;
-    } else {
-      document.documentElement.classList.remove('dark');
-      is_dark = false;
+    // check localstorage
+    if (localStorage.getItem('dark-mode')) {
+        if (localStorage.getItem('dark-mode') === 'true') {
+            document.documentElement.classList.add('dark');
+            is_dark = true;
+        } else {
+            document.documentElement.classList.remove('dark');
+            is_dark = false;
+        }
+        return;
     }
-  }
+
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.documentElement.classList.add('dark');
+        is_dark = true;
+    } else {
+        document.documentElement.classList.remove('dark');
+        is_dark = false;
+    }
+}
 checkDarkMode();
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', checkDarkMode);
 
 function toggleDarkMode() {
-  if (is_dark) {
-    document.documentElement.classList.remove('dark');
-    is_dark = false;
-  } else {
-    document.documentElement.classList.add('dark');
-    is_dark = true;
-  }
+    if (is_dark) {
+        document.documentElement.classList.remove('dark');
+        is_dark = false;
+        localStorage.setItem('dark-mode', 'false');
+    } else {
+        document.documentElement.classList.add('dark');
+        is_dark = true;
+        localStorage.setItem('dark-mode', 'true');
+    }
 }
 
 switchButton2.addEventListener("click", toggleDarkMode);
 
 switchButton.addEventListener("click", () => {
-  if (is_dark) {
-    window.location.reload(); // Esto recargará la página para aplicar el cambio de modo
-  } else {
-    toggleDarkMode();
-  }
+    if (is_dark) {
+        window.location.reload(); // Esto recargará la página para aplicar el cambio de modo
+    } else {
+        toggleDarkMode();
+    }
 });
