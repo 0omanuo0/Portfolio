17b440a2a2949a3518efaeb42428aa543697f198
0omanuo0 <manu365manu@gmail.com>
2024-07-26 20:18:55 +0000

    chore: Update for spam filtering and security enhancements

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request, redirect, send_file
 from flask_mail import Mail, Message
 from flask_sitemapper import Sitemapper
-import threading as th
 import json
 import subprocess
-import termcolor as tc
 import os
+import re
+
+
 app = Flask(__name__)
 # generate a secret key with os.urandom(24) and store it in a file
 print("------ Generating secret key... ------")
@@ -47,7 +48,7 @@ def home():
 @sm.include()
 @app.route("/curriculum")
 def curriculum():
-    return send_file("content/curriculum2.pdf")
+    return send_file("content/curriculum4.pdf")
 
 @app.route("/sitemap.xml")
 def r_sitemap():
@@ -57,12 +58,45 @@ def r_sitemap():
 def page_not_found(e):
     return render_template('page-404.html'), 404
 
+
+WHITELIST_COUNTRIES = ["FR", "ES", "US", "UK", "DE", "IT", "NL", "BE", "PT"]
+ALLOWED_LINKS = ["github", "linkedin", "twitter", "facebook"]
+BLACKLIST_WORDS = ["bitcoin", "earn", "money", "free", "click", "here", "subscribe", "buy", "sell", "trade", "crypto", "currency", "investment", "invest", "profit", "earnings", "earn", "cash", "dollar", "euro", "pound", "yen", "yuan", "rupee", "peso", "real", "ruble", "franc", "lira", "dinar", "dirham", "won", "krona", "krone", "kroner",
+                     "bitcoin", "ethereum", "ripple", "litecoin", "monero", "dash", "zcash", "dogecoin", "tron", "stellar", "cardano", "tezos", "chainlink", "polkadot", "uniswap", "aave", "maker", "compound", "yearn", "sushi", "curve", "balancer", "synthetix", "cream", "yfi", "yfii", "yfiii",
+                        "buy", "sell", "trade", "crypto", "currency", "investment", "invest", "profit", "earnings", "earn", "cash", "dollar", "euro", "pound", "yen", "yuan", "rupee", "peso", "real", "ruble", "franc", "lira", "dinar", "dirham", "won", "krona", "krone", "kroner",
+]
+
+
+
 @app.route('/submit', methods=['POST'])
 def submit():
     name, email, message = request.form['name'], request.form['email'], request.form['message']
+    
     if (not name or not email or not message):
         print("error")
         return redirect("/")
+    
+    #filter out spam check 
+    # 1. if the headers of the post request are not from a browser, the ip is not from a contry that is not in the whitelist
+    # 2. if contains words like: "bitcoin", "earn"...
+    # 3. if contains links that are not from github, linkedin, etc
+    
+    
+    
+    if any([word in message for word in BLACKLIST_WORDS]):
+        print("Blacklisted word")
+        return redirect("/")
+
+    # find links with regex:
+ 
+    links = re.findall(r'(https?://[^\s]+)', message)
+    for link in links:
+        if not any([allowed_link in link for allowed_link in ALLOWED_LINKS]):
+            print("Not allowed link")
+            return redirect("/")
+    
+    
+    
     msg = Message('From {}'.format(email), 
                     sender=email,
                     recipients=['manu365manu@gmail.com'])
