35fa2911228b019afcf7f06555ca87badbbe9b86
0omanuo0 <manu365manu@gmail.com>
2025-03-14 22:01:20 +0000

    Fix touch event handling in carr.js and add null check for panel

--- static/assets/js/carr.js
+++ static/assets/js/carr.js
@@ -7,10 +7,12 @@ var touched = false;
 
 
 function carr(direction = 0) {
-    console.log(direction);
+    if(carr == null) return;
+
+    // console.log(direction);
     if (direction > 0 && pos < cards) pos++;
     else if (direction < 0 && pos > 1) pos--;
-    console.log(pos);
+    // console.log(pos);
 
     for (let i = 1; i <= cards; i++) {
         const element = document.getElementById(i.toString());
@@ -43,52 +45,51 @@ function getScreenWidth() {
 }
 
 
+if (panel) {
+    panel.addEventListener('touchstart', (e) => {
+        let startX = 0;
+        let startY = 0;
+        let endX = 0;
+        let endY = 0;
+        let isHorizontal = false;
+        let scrolledAmount = 1;
 
-panel.addEventListener('touchstart', (e) => {
-    let startX = 0;
-    let startY = 0;
-    let endX = 0;
-    let endY = 0;
-    let isHorizontal = false;
-    let scrolledAmount = 1;
+        startX = e.touches[0].clientX;
+        startY = e.touches[0].clientY;
+        panel.addEventListener('touchend', (e) => {
+            reset();
+        });
+        panel.addEventListener('touchmove', (e) => {
+            endX = e.touches[0].clientX;
+            endY = e.touches[0].clientY;
 
-    startX = e.touches[0].clientX;
-    startY = e.touches[0].clientY;
-    panel.addEventListener('touchend', (e) => {
-        reset();
-    });
-    panel.addEventListener('touchmove', (e) => {
-        endX = e.touches[0].clientX;
-        endY = e.touches[0].clientY;
+            const deltaX = endX - startX;
+            const deltaY = endY - startY;
 
-        const deltaX = endX - startX;
-        const deltaY = endY - startY;
-
-        if (Math.abs(deltaX) > Math.abs(deltaY)) {
-            isHorizontal = true;
-            e.preventDefault(); // Bloquea el scroll vertical
-            if (scrolledAmount > 3) reset();
-            if (deltaX > 100 * scrolledAmount) {
-                carr(-1);
-                scrolledAmount++;
-            } else if (deltaX < -100 * scrolledAmount) {
-                carr(1);
-                scrolledAmount++;
+            if (Math.abs(deltaX) > Math.abs(deltaY)) {
+                isHorizontal = true;
+                e.preventDefault(); // Bloquea el scroll vertical
+                if (scrolledAmount > 3) reset();
+                if (deltaX > 100 * scrolledAmount) {
+                    carr(-1);
+                    scrolledAmount++;
+                } else if (deltaX < -100 * scrolledAmount) {
+                    carr(1);
+                    scrolledAmount++;
+                }
             }
+        });
+        const reset = () => {
+            startX = 0;
+            startY = 0;
+            endX = 0;
+            endY = 0;
+            isHorizontal = false;
+            scrolledAmount = 1;
+
+            panel.removeEventListener('touchend', null);
+            panel.removeEventListener('touchmove', null);
         }
     });
-    const reset = () => {
-        startX = 0;
-        startY = 0;
-        endX = 0;
-        endY = 0;
-        isHorizontal = false;
-        scrolledAmount = 1;
-    
-        panel.removeEventListener('touchend', null);
-        panel.removeEventListener('touchmove', null);
-    }
-});
 
-
-
+}
