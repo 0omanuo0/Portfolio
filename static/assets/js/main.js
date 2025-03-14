d354bb823eb79b515367c3368ceb54dc082d7202
0omanuo0 <manu365manu@gmail.com>
2025-03-14 22:01:04 +0000

    Remove shiny

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,46 +1,59 @@
-let shinyElement = document.querySelector(".shiny");
-// shinyElement = null;
 
 
-shinyElement.addEventListener("mouseenter", () => {
-    // set variable --transition-speed to 0.1s
-    if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
-        shinyElement.style.setProperty("--transition-speed", "0.1s");
-    }
+function main() {
+    let shinyElement = document.querySelector(".shiny");
+    shinyElement = null;
 
-    shinyElement.classList.remove("mouse-leave");
-});
+    if (!shinyElement) return;
 
-const updateShinePosition = (e) => {
-    const { x, y } = shinyElement.getBoundingClientRect();
-    shinyElement.style.setProperty("--x", e.clientX - x);
-    shinyElement.style.setProperty("--y", e.clientY - y);
+    shinyElement.addEventListener("mouseenter", () => {
+        // if is mobile or prefers-reduced-motion is enabled, don't update the position
+        if (window.matchMedia("(prefers-reduced-motion: reduce)").matches || window.matchMedia("(max-width: 768px)").matches) {
+            return;
+        }
 
-    // Si el cursor está sobre una imagen, esconder el brillo
-    if (e.target.tagName === "IMG") {
-        shinyElement.classList.add("mouse-leave");
-    } 
-    else {
+
+        // set variable --transition-speed to 0.1s
+        if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+            shinyElement.style.setProperty("--transition-speed", "0.1s");
+        }
+
         shinyElement.classList.remove("mouse-leave");
-    }
+    });
 
-    // Si el cursor está sobre un INPUT, BUTTON, o un elemento con id "title", aumentar el brillo
-    if (e.target.tagName === "INPUT" || e.target.tagName === "BUTTON" || e.target.getAttribute("key") === "title" || e.target.tagName === "TEXTAREA") {
-        shinyElement.style.setProperty("--scale-s", 1.5);
-    } 
-    else {
-        shinyElement.style.setProperty("--scale-s", 1);
-    }
-};
-// Listener para el movimiento del ratón
-shinyElement.addEventListener("mousemove", (e) => {
-    // if prefers-reduced-motion is enabled, don't update the position
-    if (window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-        return;
-    }
-    updateShinePosition(e);
-});
+    const updateShinePosition = (e) => {
+        const { x, y } = shinyElement.getBoundingClientRect();
+        shinyElement.style.setProperty("--x", e.clientX - x);
+        shinyElement.style.setProperty("--y", e.clientY - y);
 
-shinyElement.addEventListener("mouseleave", () => {
-    shinyElement.classList.add("mouse-leave");
-});
+        // Si el cursor está sobre una imagen, esconder el brillo
+        if (e.target.tagName === "IMG") {
+            shinyElement.classList.add("mouse-leave");
+        }
+        else {
+            shinyElement.classList.remove("mouse-leave");
+        }
+
+        // Si el cursor está sobre un INPUT, BUTTON, o un elemento con id "title", aumentar el brillo
+        if (e.target.tagName === "INPUT" || e.target.tagName === "BUTTON" || e.target.getAttribute("key") === "title" || e.target.tagName === "TEXTAREA") {
+            shinyElement.style.setProperty("--scale-s", 1.5);
+        }
+        else {
+            shinyElement.style.setProperty("--scale-s", 1);
+        }
+    };
+    // Listener para el movimiento del ratón
+    shinyElement.addEventListener("mousemove", (e) => {
+        // if prefers-reduced-motion is enabled, don't update the position
+        if (window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+            return;
+        }
+        updateShinePosition(e);
+    });
+
+    shinyElement.addEventListener("mouseleave", () => {
+        shinyElement.classList.add("mouse-leave");
+    });
+}
+
+main();
